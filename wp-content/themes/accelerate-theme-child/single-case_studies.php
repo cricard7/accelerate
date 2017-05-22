0da91ff0f3979ccb715fd6f3045a0c741eb40266
cricard7 <chad_ricard@hotmail.com>
2017-05-22 13:41:12 -0500

    added plugin advanced custom fields. Updated Case studies to have custom fields and the single-case_studies.php template to have the html template that now calls the fields

--- wp-content/themes/accelerate-theme-child/single-case_studies.php
+++ wp-content/themes/accelerate-theme-child/single-case_studies.php
@@ -10,15 +10,76 @@ get_header(); ?>
 
 
 
-	<div id="primary" class="site-content">
-		<div id="content" role="main">
-        
-     
-			<?php while ( have_posts() ) : the_post(); ?>
-				<?php the_content(); ?>
-			<?php endwhile; // end of the loop. ?>
+    <div id="primary" class="site-content">
+        <div id="content" role="main">
 
-		</div><!-- #content -->
-	</div><!-- #primary -->
 
-<?php get_footer(); ?>
+
+
+
+            <?php while ( have_posts() ) : the_post(); 
+            
+            $title = get_field('title'); 
+            $client = get_field('client_name'); 
+            $link = get_field('link'); 
+            // $img1 and 2 are using image url in custom fields pluging
+            $img1 = get_field('image1'); 
+            $img2 = get_field('image2'); 
+            // $img3 is using the image id in custom fields plugin
+            $img3 = get_field('image3');
+            $size = "full";
+            ?>
+
+
+
+
+                <aside class="caseStudyDescription">
+                    <h3><?php echo $title ?></h3>
+                    <h4>Client: <?php echo $client; ?></h4>
+                    <p>
+                        <?php the_content(); ?>
+                    </p>
+
+                    <a href="<?php echo $link ?>">Visit Live Site</a>
+                </aside>
+
+                <div class="caseStudyImages">
+                    <?php if($img1) { ?>
+                        <img src="<?php echo $img1 ?>" />
+                      <?php  } ?>
+                    
+                     <?php if($img2) { ?>
+                        <img src="<?php echo $img2 ?>" />
+                      <?php  } ?>
+                    
+                    <!-- wp_get_attachment_image()  this is the preferred method of getting images as it takes advantange of wordress captions and sizes -->    
+                     <?php if($img3) { ?>
+                        
+                    <?php echo wp_get_attachment_image($img3, $size); ?>
+
+                      <?php  } ?>
+                       
+                
+                    
+                    
+                </div>
+
+
+
+
+
+                <?php endwhile; // end of the loop. ?>
+
+
+
+
+
+
+
+
+        </div>
+        <!-- #content -->
+    </div>
+    <!-- #primary -->
+
+    <?php get_footer(); ?>
