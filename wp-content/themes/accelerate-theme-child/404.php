25630da07fbd71594516a63fc2005e58dd067804
cricard7 <chad_ricard@hotmail.com>
2017-05-30 23:51:57 -0500

    added custom 404.php page

---
+++ wp-content/themes/accelerate-theme-child/404.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site will use a
+ * different template.
+ *
+ * @package WordPress
+ * @subpackage Accelerate Marketing
+ * @since Accelerate Marketing 1.0
+ */
+
+get_header(); ?>
+
+	<div id="primary" class="site-content">
+		<div id="content" role="main">
+       
+        <h2 class="fourOfour">404!!!</h2>
+        <h3>Ohhhh No... You definitely broke something....</h3>
+        <p>Rest assured that a team of oompa loompas have been dispatched to investigate.</p>
+      
+        <p>While you wait, feel free to take a look around our <a class="greenText" href="#">blog</a> or some of our featured <a class="greenText" href="#">work</a></p>
+
+		</div><!-- #content -->
+	</div><!-- #primary -->
+
+
+<?php get_footer(); ?>
